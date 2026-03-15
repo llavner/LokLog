6425ad99c5d45b72b7f76dff1043c760e4f38646
llavner <marcus.renvall@gmail.com>
2026-03-15 17:26:04 +0100

    updated OvertimeEntry

--- src/LokLog.Core/Models/OvertimeEntry.cs
+++ src/LokLog.Core/Models/OvertimeEntry.cs
@@ -1,25 +1,25 @@
 namespace LokLog.Core.Models;
 
-internal class OvertimeEntry(int id, Guid userId, DateTime date, decimal minutes, string description, OvertimeCategory category, DateTime createdAt, DateTime updatedAt)
+public class OvertimeEntry(int id, Guid userId, DateTime date, decimal minutes, string description, OvertimeCategory category, DateTime createdAt, DateTime updatedAt)
 {
-    internal int Id { get; set; } = id;
+    public int Id { get; set; } = id;
 
-    required internal Guid UserId { get; set; } = userId;
+    required public Guid UserId { get; set; } = userId;
 
-    internal DateTime Date { get; set; } = date;
+    public DateTime Date { get; set; } = date;
 
-    internal decimal Minutes { get; set; } = minutes;
+    public decimal Minutes { get; set; } = minutes;
 
-    internal string Description { get; set; } = description;
+    public string Description { get; set; } = description;
 
-    internal OvertimeCategory Category { get; set; } = category;
+    public OvertimeCategory Category { get; set; } = category;
 
-    internal DateTimeOffset CreatedAt { get; set; } = createdAt;
+    public DateTimeOffset CreatedAt { get; set; } = createdAt;
 
-    internal DateTimeOffset? UpdatedAt { get; set; } = updatedAt;
+    public DateTimeOffset? UpdatedAt { get; set; } = updatedAt;
 }
 
-internal enum OvertimeCategory
+public enum OvertimeCategory
 {
     Regular,
     Holiday,
