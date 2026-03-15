a4d84aecc361674d2e454d37e914c6371131222e
llavner <marcus.renvall@gmail.com>
2026-03-15 17:07:31 +0100

    updated editorconf and removed comments in api and gui

--- src/LokLog.Gui/Program.cs
+++ src/LokLog.Gui/Program.cs
@@ -6,13 +6,11 @@ internal class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
         builder.Services.AddRazorComponents()
             .AddInteractiveServerComponents();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Error", createScopeForErrors: true);
