511ab14b05fb7efe1c573dcc257586662e9b3af2
llavner <marcus.renvall@gmail.com>
2026-03-15 11:51:09 +0100

    trying to fix stylecop warnings

--- tests/LokLog.Tests/UnitTest1.cs
+++ tests/LokLog.Tests/UnitTest1.cs
@@ -1,11 +1,9 @@
-﻿namespace LokLog.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace LokLog.Tests;
 
-        }
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
     }
 }
